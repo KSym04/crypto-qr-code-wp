f4226af83aa961cddf5a1f0c93e5d91be5dcfbb9
LAPTOP-AR62V6PB\pc <dev@dopethemes.com>
2019-03-24 23:19:21 +0800

    Added language and shortcode.

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1,3 +1,25 @@
 <?php
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly.
+
+function crypto_qr_code_wp_shortcode_logic( $atts ) {
+	// Parse shortcode attributes.
+	$a = shortcode_atts( array(
+		'label' => '',
+		'address' => ''
+    ), $atts );
+    
+    // Set variables.
+    $content = NULL;
+    settype( $content, 'string' );
+
+    // Build template.
+    $content .= '<div>
+                    <a href="#'. $a['address'] .'" title="'. $a['label'] .' '. $a['address'] .'">
+                        <label>'. $a['label'] .'</label>: '. $a['address'] .'
+                    </a>
+                 </div>';
+
+    return $content;
+}
+add_shortcode( 'cqcw_generator', 'crypto_qr_code_wp_shortcode_logic' );
